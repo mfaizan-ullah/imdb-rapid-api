7ae2d93498404d1af5e267ab0ece59616a32c21a
mfaizan-ullah <49563378+mfaizan-ullah@users.noreply.github.com>
2021-06-18 20:20:29 +0500

    Create imdb.py

---
+++ imdb.py
@@ -0,0 +1,38 @@
+import http.client
+import urllib.request
+from bs4 import BeautifulSoup
+
+conn = http.client.HTTPSConnection("imdb8.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "e2e672d7a1msh0d8c6e6fc41552dp123d66jsnbbdcedb777cb",
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+conn.request("GET", "/title/get-most-popular-movies?homeCountry=GB&purchaseCountry=GB&currentCountry=GB", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+namescode=[]
+namesfetch=[]
+retlist=data.decode("utf-8")
+names=retlist.split(',')
+for name in names:
+    namecode=name.split('/')
+    namescode.append(namecode[2])
+i=0
+souplist=[]
+while(i<3):
+    souplist.append(namescode[i])
+    i=i+1
+# Now fetch the name of top three
+topthree=[]
+for nam in souplist:
+    url="https://www.imdb.com/title/"+str(nam)+"/"
+    link=urllib.request.urlopen(url)
+    data=link.read()
+    mydata=data.decode("utf-8")
+    link.close()
+    soup=BeautifulSoup(mydata, 'html.parser')
+    name=soup.find("title")
+    moviname=name.text.strip()
+    print(moviname,url)
